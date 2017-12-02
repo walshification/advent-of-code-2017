f323e6ae2d163dbd86f16170bfbbf22e4cf737f1
Christopher Walsh <walshification@gmail.com>
2017-12-02 13:17:30 -0600

    refactors for better abstraction

--- solutions/captcha.py
+++ solutions/captcha.py
@@ -1,9 +1,14 @@
 def solve(sequence):
-    sequence = str(sequence)
-    pairs = []
-    sequence_end = len(sequence)
-    for i in range(sequence_end):
-        next_i = i + 1 if i + 1 is not (sequence_end) else 0
-        if sequence[i] == sequence[next_i]:
-            pairs.append(int(sequence[i]))
-    return sum(pairs)
+    return sum(_digits_with_a_following_pair(sequence))
+
+
+def _digits_with_a_following_pair(sequence):
+    sequence_str = str(sequence)
+    return [digit_with_pair(i, sequence_str) for i in range(len(sequence_str))]
+
+
+def digit_with_pair(i, sequence_str):
+    next_i = i + 1 if i + 1 is not len(sequence_str) else 0
+    if sequence_str[i] == sequence_str[next_i]:
+        return int(sequence_str[i])
+    return 0
