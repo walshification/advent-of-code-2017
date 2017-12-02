805c9679ba44ca3d6809450dab992813acced882
Christopher Walsh <walshification@gmail.com>
2017-12-02 12:20:17 -0600

    adds tests to round out the solution edge cases

--- tests/test_captcha.py
+++ tests/test_captcha.py
@@ -7,3 +7,15 @@ class CaptchaTests(unittest.TestCase):
     def test_sums_repeating_number_pairs(self):
         solution = captcha.solve(1122)
         self.assertEqual(solution, 3)
+
+    def test_sums_overlapping_repeating_pairs(self):
+        solution = captcha.solve(1111)
+        self.assertEqual(solution, 4)
+
+    def test_skips_pairs_that_do_not_match(self):
+        solution = captcha.solve(1234)
+        self.assertEqual(solution, 0)
+
+    def test_counts_last_digit_if_it_matches_the_first(self):
+        solution = captcha.solve(91212129)
+        self.assertEqual(solution, 9)
