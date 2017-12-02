745d03aa37a30a7a2f84c1384a6a0567d03f3dc3
Christopher Walsh <walshification@gmail.com>
2017-12-02 16:55:23 -0600

    adds solution script

--- solutions/checksum.py
+++ solutions/checksum.py
@@ -1,3 +1,6 @@
+import yaml
+
+
 def calculate(rows):
     return sum([max_min_difference(row) for row in rows])
 
@@ -5,3 +8,9 @@ def calculate(rows):
 def max_min_difference(row):
     digits = [digit for digit in row if digit is not None]
     return max(digits) - min(digits)
+
+
+if __name__ == '__main__':
+    with open('solutions/problem_inputs/checksum.yaml', 'r') as spreadsheet:
+        test_input = yaml.load(spreadsheet)
+    print('Part One: ', calculate(test_input))
