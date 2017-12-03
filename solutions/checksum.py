6872679be57d00dea1fa8c8ac48028ecc6e42718
Christopher Walsh <walshification@gmail.com>
2017-12-02 18:11:07 -0600

    makes calculation composable and solves part two

--- solutions/checksum.py
+++ solutions/checksum.py
@@ -1,8 +1,8 @@
 import yaml
 
 
-def calculate(rows):
-    return sum([max_min_difference(row) for row in rows])
+def calculate(rows, difference_func):
+    return sum([difference_func(row) for row in rows])
 
 
 def max_min_difference(row):
@@ -10,7 +10,20 @@ def max_min_difference(row):
     return max(digits) - min(digits)
 
 
+def even_divisor_difference(row):
+    divisors = [digit for digit in row if _divides_evenly(digit, row)]
+    return int(max(divisors) / min(divisors))
+
+
+def _divides_evenly(number, digits):
+    for digit in digits:
+        if number != digit and (number % digit == 0 or digit % number == 0):
+            return True
+    return False
+
+
 if __name__ == '__main__':
     with open('solutions/problem_inputs/checksum.yaml', 'r') as spreadsheet:
         test_input = yaml.load(spreadsheet)
-    print('Part One: ', calculate(test_input))
+    print('Part One: ', calculate(test_input, max_min_difference))
+    print('Part Two: ', calculate(test_input, even_divisor_difference))
--- tests/test_checksum.py
+++ tests/test_checksum.py
@@ -1,14 +1,33 @@
 import unittest
 
-from solutions import checksum
+from solutions.checksum import calculate, max_min_difference, even_divisor_difference
 
 
-class ChecksumTests(unittest.TestCase):
-    def test_sums_repeating_number_pairs(self):
+class CalculateTests(unittest.TestCase):
+    def test_calculates_checksum_from_min_max_difference_in_rows(self):
         test_spreadsheet = [
             [5, 1, 9, 5],
             [7, 5, 3, None],
             [2, 4, 6, 8]
         ]
-        solution = checksum.calculate(test_spreadsheet)
+        solution = calculate(test_spreadsheet, max_min_difference)
         self.assertEqual(solution, 18)
+
+    def test_calculates_checksum_from_even_divisor_difference_in_rows(self):
+        test_spreadsheet = [
+            [5, 9, 2, 8],
+            [9, 4, 7, 3],
+            [3, 8, 6, 5],
+        ]
+        solution = calculate(test_spreadsheet, even_divisor_difference)
+        self.assertEqual(solution, 9)
+
+
+class EvenDivisorTests(unittest.TestCase):
+    def test_even_divisor_divides_two_digits_if_they_divide_evenly(self):
+        quotient = even_divisor_difference([12, 6])
+        self.assertEqual(quotient, 2)
+
+    def test_even_divisor_skips_numbers_that_dont_evenly_divide(self):
+        quotient = even_divisor_difference([18, 7, 5, 9])
+        self.assertEqual(quotient, 2)
