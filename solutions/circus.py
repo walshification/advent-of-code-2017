eca6a6db7d30ccf7192d69dd1ef9dce8a37affe1
Christopher Walsh <walshification@gmail.com>
2017-12-09 01:29:13 -0600

    finds off-balance program

--- solutions/circus.py
+++ solutions/circus.py
@@ -1,4 +1,6 @@
 import re
+from itertools import chain
+
 import yaml
 
 
@@ -21,10 +23,65 @@ class Pyramid:
     def __init__(self, programs):
         self.tower = self._build_tower(programs)
 
+    @classmethod
+    def find_unbalanced_program(cls, pyramid, root):
+        child_lineages = cls.__map_children_to_lineages(pyramid, root)
+        lineage_weights = cls.__map_children_to_total_weights(child_lineages)
+        weight_names = cls.__map_weights_to_names(lineage_weights)
+
+        if pyramid.is_unbalanced(weight_names):
+            off_balance_program = cls.__find_off_balance_program(weight_names.values())
+            return cls.find_unbalanced_program(pyramid, pyramid.tower[off_balance_program])
+        else:
+            return root.name
+
+    @classmethod
+    def __map_children_to_lineages(cls, pyramid, root):
+        return {
+            child: [pyramid.tower[child]] + pyramid.descendants(child)
+            for child in root.children
+        }
+
+    @classmethod
+    def __map_children_to_total_weights(cls, child_lineages):
+        return {
+            child: sum(child.weight for child in lineage)
+            for child, lineage in child_lineages.items()
+        }
+
+    @classmethod
+    def __map_weights_to_names(cls, lineage_weights):
+        weight_names = {}
+        for name, weight in lineage_weights.items():
+            if weight_names.get(weight):
+                weight_names[weight].append(name)
+            else:
+                weight_names[weight] = [name]
+        return weight_names
+
+    @classmethod
+    def __find_off_balance_program(cls, names):
+        for name_list in names:
+            if len(name_list) == 1:
+                return name_list[0]
+
     @property
     def root(self):
         any_key = next(iter(self.tower))
         return self._find_root(self.tower[any_key])
+
+    def descendants(self, program):
+        if not self.tower[program].children:
+            return []
+
+        children = [
+            [self.tower[child]] + self.descendants(child)
+            for child in self.tower[program].children
+        ]
+        return list(chain.from_iterable(children))  # flatten the lists
+
+    def is_unbalanced(self, weight_names):
+        return len(weight_names) > 1
 
     def _build_tower(self, programs):
         tower = self._construct_programs(programs)
@@ -46,7 +103,7 @@ class Pyramid:
 
     def _find_root(self, program):
         if not program.parent:
-            return program.name
+            return program
         return self._find_root(self.tower[program.parent])
 
 
--- tests/test_circus.py
+++ tests/test_circus.py
@@ -28,4 +28,47 @@ class ParentTests(unittest.TestCase):
             'foo (17) -> asdf',
             'asdf (34) -> qwer',
         ])
-        self.assertEqual(pyramid.root, 'foo')
+        self.assertEqual(pyramid.root.name, 'foo')
+
+    def test_descendants_returns_list_of_child_programs_of_program(self):
+        pyramid = Pyramid([
+            'qwer (12)',
+            'foo (17) -> asdf',
+            'asdf (34) -> qwer',
+        ])
+        children = [pyramid.tower['asdf'], pyramid.tower['qwer']]
+        self.assertEqual(pyramid.descendants('foo'), children)
+
+    def test_descendants_returns_list_with_siblings_of_program(self):
+        pyramid = Pyramid([
+            'qwer (12)',
+            'uiop (43)',
+            'foo (17) -> asdf, bar',
+            'bar (57) -> uiop',
+            'asdf (34) -> qwer',
+        ])
+        children = [
+            pyramid.tower['asdf'],
+            pyramid.tower['qwer'],
+            pyramid.tower['bar'],
+            pyramid.tower['uiop'],
+        ]
+        self.assertEqual(pyramid.descendants('foo'), children)
+
+    def test_find_unbalanced_program_returns_off_kilter_program(self):
+        pyramid = Pyramid([
+            'pbga (66)',
+            'xhth (57)',
+            'ebii (61)',
+            'havc (66)',
+            'ktlj (57)',
+            'fwft (72) -> ktlj, cntj, xhth',
+            'qoyq (66)',
+            'padx (45) -> pbga, havc, qoyq',
+            'tknk (41) -> ugml, padx, fwft',
+            'jptl (61)',
+            'ugml (68) -> gyxo, ebii, jptl',
+            'gyxo (61)',
+            'cntj (57)',
+        ])
+        self.assertEqual(Pyramid.find_unbalanced_program(pyramid, pyramid.root), 'ugml')
