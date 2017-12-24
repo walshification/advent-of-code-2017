5f4db53faeea4d81655f62ad4e877dc7cc65970c
Christopher Walsh <walshification@gmail.com>
2017-12-24 12:27:15 -0600

    solves part one

--- solutions/knot_hash.py
+++ solutions/knot_hash.py
@@ -28,3 +28,9 @@ class KnotHash:
         self._string.rotate(-self.current_position)
         self._string.extendleft([self._string.popleft() for i in range(length)])
         self._string.rotate(self.current_position)
+
+
+if __name__ == '__main__':
+    test_input = [189, 1, 111, 246, 254, 2, 0, 120, 215, 93, 255, 50, 84, 15, 94, 62]
+    knot = KnotHash(test_input).twist()
+    print('Part One:', knot[0] * knot[1])
